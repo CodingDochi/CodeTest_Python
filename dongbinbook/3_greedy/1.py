5ad52669ad517c260331e1d66d459a26b152ef62
CodingDochi <cheers7129@gmail.com>
2024-09-26 04:45:28 +0900

    .

--- dongbinbook/3_greedy/1.py
+++ dongbinbook/3_greedy/1.py
@@ -6,6 +6,6 @@ coins = [500, 100, 50, 10]
 
 for coin in coins :
     cnt += n // coin # 해당 화폐로 거슬러 줄 수 있는 동전 개수 세기
-    n %= coin
+    n %= coin # n을 coin으로 나눈 나머지만 남기고, 그 값을 다시 n에 저장
 
 print(cnt)
